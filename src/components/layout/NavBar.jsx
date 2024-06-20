18574332b7059a61f86f942e20dd4368bbfc09cd
Pham Van Thanh <thanhdever@gmail.com>
2024-06-20 22:29:40 +0700

    fix title

--- src/components/layout/NavBar.jsx
+++ src/components/layout/NavBar.jsx
@@ -17,7 +17,7 @@ const NavBar = () => {
 		<nav className="navbar navbar-expand-lg bg-body-tertiary px-5 shadow mt-5 sticky-top">
 			<div className="container-fluid">
 				<Link to={"/"} className="navbar-brand">
-					<span className="hotel-color">lakeSide Hotel</span>
+					<span className="hotel-color">LakeSide Hotel</span>
 				</Link>
 
 				<button
